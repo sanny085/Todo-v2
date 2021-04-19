a94bc7cff8b251493915a0b0251373814dd2ec8d
Sanny Kumar <sannykumar085@gmail.com>
2021-04-19 22:48:11 +0530

    return missing

--- src/context/reducer.js
+++ src/context/reducer.js
@@ -1,14 +1,13 @@
 import { ADD_TODO , REMOVE_TODO } from "./action.types";
 
-export default (state, action) =>{
+export default (state, action) => {
     switch(action.type) {
         case ADD_TODO:
-            return [...state, action.payload]
+            return [...state, action.payload];
         case REMOVE_TODO:
             return state.filter(todo => todo.id != action.payload);
         
         default:
-             state;
+            return state;
     }
-
 }
