a348e013341d6d74889e24811b606924608bd292
SANNY KUMAR <49550883+sanny085@users.noreply.github.com>
2021-04-20 10:51:08 +0530

    Update reducer.js

--- src/context/reducer.js
+++ src/context/reducer.js
@@ -6,7 +6,6 @@ export default (state, action) => {
             return [...state, action.payload];
         case REMOVE_TODO:
             return state.filter(todo => todo.id != action.payload);
-        
         default:
             return state;
     }
