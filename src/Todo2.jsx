e73c7ba64c499a547096002b9c3dc5fee205db3b
SANNY KUMAR <49550883+sanny085@users.noreply.github.com>
2021-04-20 10:49:20 +0530

    Update Todo2.jsx

--- src/Todo2.jsx
+++ src/Todo2.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
+import React,{ useReducer } from 'react'
+import { Container } from 'reactstrap';
+
+import TodoContext from './context/TodoContext';
+import todoReducer from './context/reducer';
+
 
 const Todo2 = () => {
+    const [todos, dispatch] = useReducer(todoReducer, [])
     return (
-        <div>
-            Todo version 2.0
-        </div>
+        <>
+        <TodoContext.Provider value={{ todos, dispatch }}>
+         <Container fluid>
+             <h1>Todo App With Context API (useReducer)</h1>
+         </Container>
+        </TodoContext.Provider>
+        </>
     )
 }
 
