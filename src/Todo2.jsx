3b4ed651855dfd3eb2e6f53b863a8f37a8e2f6d6
SANNY KUMAR <49550883+sanny085@users.noreply.github.com>
2021-04-21 12:29:53 +0530

    Update Todo2.jsx

--- src/Todo2.jsx
+++ src/Todo2.jsx
@@ -1,4 +1,4 @@
-import React,{ useReducer } from 'react'
+import React,{ useReducer, useEffect } from 'react'
 import { Container } from 'reactstrap';
 
 import TodoContext from './context/TodoContext';
@@ -8,19 +8,25 @@ import TodoForm from './components/TodoForm';
 import Todos from './components/Todos';
  
 const Todo2 = () => {
-    const [todos, dispatch] = useReducer(todoReducer, []);
+    const [todos, dispatch] = useReducer(todoReducer, [], () =>{
+        const localdata = localStorage.getItem('todos');
+        return localdata ? JSON.parse(localdata) : [];
+    });
+
+    useEffect( () => {
+        localStorage.setItem('todos', JSON.stringify(todos));
+    },[todos])
     return (
-        <>
-        <TodoContext.Provider value={{ todos, dispatch }}>
+     <>
+      <TodoContext.Provider value={{ todos, dispatch }}>
          <Container fluid>
              <h1>Todo App With Context API (useReducer)</h1>
              <Todos/>
              <TodoForm/>
 
          </Container>
-        </TodoContext.Provider>
-        </>
+       </TodoContext.Provider>
+     </>
     )
 }
 export default Todo2;
-
