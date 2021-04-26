76a0dd2ec086399089c18f641798096ab32fd950
SANNY KUMAR <49550883+sanny085@users.noreply.github.com>
2021-04-26 15:26:10 +0530

    Update reducer.js

--- src/context/reducer.js
+++ src/context/reducer.js
@@ -1,13 +1,15 @@
 import { ADD_TODO , REMOVE_TODO } from "./action.types";
 
-export default (state, action) => {
+const todoReducer = (state, action) => {
     switch(action.type) {
         case ADD_TODO:
             return [...state, action.payload];
         case REMOVE_TODO:
-            return state.filter(todo => todo.id != action.payload);
+            return state.filter(todo => todo.id !== action.payload);
         
         default:
             return state;
     }
 }
+
+export default todoReducer;
